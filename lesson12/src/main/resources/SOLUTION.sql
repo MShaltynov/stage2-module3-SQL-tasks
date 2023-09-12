c689054728750fadd6e3709a976f44cc8350d529
Mikhail <shaltynovm@gmail.com>
2023-09-12 16:32:57 +0530

    fis errors

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,4 @@
-DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE grade >= 4);
-DELETE FROM student WHERE grade >= 4;
-DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4));
-DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
-DELETE FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'Daily');
-DELETE FROM paymenttype WHERE name = 'Daily';
-DELETE FROM mark WHERE mark < 7;
+DELETE FROM mark WHERE student_id IN (select student_id from student join mark on student_id=mark.student_id where mark>=4);
+DELETE FROM mark WHERE student_id IN (select student_id from student join mark on student_id=mark.student_id where mark<4);
+UPDATE PAYMENT SET "TYPE_ID"=0 WHERE TYPE_ID=1;DELETE FROM PAYMENTTYPE WHERE NAME='DAILY';
+DELETE FROM MARK WHERE MARK<7;
