4229adf7bcab60883a99e9c8956b2f39f660f222
Mikhail <shaltynovm@gmail.com>
2023-09-13 12:02:15 +0530

    fixed lesson 6

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 Select * from payment where TYPE_ID =(SELECT id from paymenttype where name='MONTHLY');
 SELECT mark.* from mark join subject ON mark.subject_id=subject.id  where name like '%Art%';
-SELECT student.* from student join  payment ON student.id=payment.student_id where type_id=2;
+select * from student JOIN (select distinct STUDENT_ID  from payment where type_id=(SELECT id from  PAYMENTTYPE where UPPER(name) LIKE'WEEKLY')) as paid ON paid.student_id=student.id;
 SELECT * from student join (select student_id from mark join subject on mark.subject_id=subject.id where name like '%Math%') a on student.id=a.student_id;
